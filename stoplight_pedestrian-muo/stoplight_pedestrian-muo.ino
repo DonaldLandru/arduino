1e557e4922790e1c8d771832a70a8804dbe7bd02
Donald Landru <donald.landru@btmglobal.com>
2017-01-14 11:53:35 -0600

    Make changes to code, specifically yellow status during transitions. Cleaned up code for readability

--- stoplight_pedestrian-muo/stoplight_pedestrian-muo.ino
+++ stoplight_pedestrian-muo/stoplight_pedestrian-muo.ino
@@ -1,45 +1,60 @@
-int red = 10;
-int yellow = 9;
+// variables
+/// Name pins to lights
 int green = 8;
-int button = 12; // switch is on pin 12
+int yellow = 9;
+int red = 10;
+/// specify button location
+int button = 12;
 
-void setup(){
-    pinMode(red, OUTPUT);
-    pinMode(yellow, OUTPUT);
-    pinMode(green, OUTPUT);
-    pinMode(button, INPUT);
-    digitalWrite(green, HIGH);
+
+// basic functions
+void setup()
+{
+  /// setup PIN modes
+  // specifying LED as output 
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(red,OUTPUT);
+  
+  // specifying button as input
+  pinMode(button,INPUT);
+  
+  // set green LED on by default
+  digitalWrite(green,HIGH);
 }
 
-void loop() {
-    if (digitalRead(button) == HIGH){
-        delay(15); // software debounce
-        if (digitalRead(button) == HIGH) {
-            // if the switch is HIGH, ie. pushed down - change the lights!
-            changeLights();
-            delay(15000); // wait for 15 seconds
-        }
-    }
+// wait for button input, software debounce, and repeat delay
+void loop()
+{
+  if (digitalRead(button) == HIGH) {
+    delay(15); //software debounce
+     if (digitalRead(button) == HIGH) {
+      // if the switch is HIGH, ie. pushed down -- change the lights!
+      changeLights();
+      delay(15000); // wait for 15 seconds
+     }
+  }
 }
 
-void changeLights()
-    // green off, yellow on for 3 seconds
-    digitalWrite(green, LOW);
-    digitalWrite(yellow, HIGH);
-    delay(3000);
 
-    // turn off yellow, then turn red on for 5 seconds
-    digitalWrite(yellow, LOW);
-    digitalWrite(red, HIGH);
-    delay(5000);
+// function changeLights(), code responisble for controlling lights
+void changeLights() 
+{
+  // green off, yellow on for 3 seconds
+  digitalWrite(green,LOW);
+  digitalWrite(yellow,HIGH);
+  delay(3000);
 
-    // red and yellow on for 2 seconds (red is already on though)
-    digitalWrite(yellow, HIGH);
-    delay(2000);
+  // turn off yellow, then turn red on for 5 seconds
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,HIGH);
+  delay(5000);
 
-    // turn off red and yellow, then turn on green
-    digitalWrite(yellow, LOW);
-    digitalWrite(red, LOW);
-    digitalWrite(green, HIGH);
-    delay(3000);
+  // turn off red and yellow, then turn on green
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  delay(3000);
 }
+
+
